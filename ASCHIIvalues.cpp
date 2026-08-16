9cdd7eb852dd72e609e39a2649ba0dbb214f8b83
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-16 23:14:00 +0530

    ASCHII Value

---
+++ ASCHIIvalues.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+   char ch;
+   cout<<"ch : ";
+   cin>>ch;
+   cout<<(int)ch;
+}
--- typecasting.cpp
+++ typecasting.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() {
     int x;
+    cout<<"x : ";
     cin>>x;
     float y = (float) x;
     cout<<x/13;
