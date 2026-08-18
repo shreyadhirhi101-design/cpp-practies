33a3291792adcd859b65742c3f8392d7ca879a80
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-18 10:33:06 +0530

    1st row

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int a11;
+    cout<<"a12 : ";
+    cin>>a11;
+
+    int a12;
+    cout<<"a12 : ";
+    cin>>a12;
+
+    int a13;
+    cout<<"a13 : ";
+    cin>>a13;
 }
