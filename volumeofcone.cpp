0d30f673054400025da155de36e7bd681105d953
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-19 22:25:43 +0530

    Volume of cone

--- volumeofcone.cpp
+++ volumeofcone.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    float r;
-    cout<<"r : ";
-    cin>>r;
+    float r;{
+        cout<<"r : ";
+        cin>>r;
+    }
 
-    float h;
-    cout<<"h : ";
-    cin>>h;
+    float h;{
+        cout<<"h : ";
+        cin>>h;
+    }
+
+    cout<<"Volume of cone : ";
+    cout<<1.0/3.0*3.1415*r*r*h;
 }
