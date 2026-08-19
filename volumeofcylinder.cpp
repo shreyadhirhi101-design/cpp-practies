947bdb256f1755b7de2e3f3d857bc6401de502cc
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-19 22:48:35 +0530

    Volume of cylinder

---
+++ volumeofcylinder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;{
+        cout<<"r : ";
+        cin>>r;
+    }
+
+    float h;{
+        cout<<"h : ";
+        cin>>h;
+    }
+
+    cout<<"Volume of cylinder : ";
+    cout<<3.1415*r*r*h;
+}
