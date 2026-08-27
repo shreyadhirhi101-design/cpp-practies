0364a42885510f026b5f5747a2a464095af7c198
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-27 22:16:57 +0530

    Area of rect. and Perimeter

---
+++ areaofrectangleandperimeter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float l;{
+        cout<<"Length : " ;
+    cin>>l;
+    }
+    float b;{
+        cout<<"Breadth : " ;
+    cin>>b;
+    }
+    float A;{
+        cout<<"Area of rectangle : "<<l*b;
+    }
+    float P;{
+        cout<<"Perimeter : "<<(l*b)*2;
+    }
+}
