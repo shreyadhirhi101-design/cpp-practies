dde17a2924d00735fcb440d2900b3b5b461ffd4e
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-27 23:57:14 +0530

    functiona a b and c for the triangle

---
+++ sidesofatriangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int a;
+   cout<<"a : ";
+   cin>>a;
+
+   int b;
+   cout<<"b : ";
+   cin>>b;
+
+   int c;
+   cout<<"c : ";
+   cin>>c;
+
+}
