63aff0f69e47402645e7ea64e9bb81ade8043397
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-28 12:39:20 +0530

    Removing curly brackets

--- printgreatestin3num.cpp
+++ printgreatestin3num.cpp
@@ -1,19 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a;{
-        cout<<"a : ";
-        cin>>a;
-    }
-    int b;{
-        cout<<"b : ";
-        cin>>b;
-    }
-    int c;{
-        cout<<"c : ";
-        cin>>c;
-    }
-
+    int a;
+    cout<<"a : ";
+    cin>>a;
+    int b;
+    cout<<"b : ";
+    cin>>b;
+    
+    int c;
+    cout<<"c : ";
+    cin>>c;
+    
     if(a>b && a>c){
         cout<<"a is greatest";
     }
