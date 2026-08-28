01fd2e7f9080f5d29aceb1e97722101425b2de85
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-28 12:04:53 +0530

    greatest num

---
+++ printgreatestin3num.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;{
+        cin>>a;
+    }
+    int b;{
+        cin>>b;
+    }
+    int c;{
+        cin>>c;
+    }
+
+    if(a>b & a>c){
+        cout<<"a is greatest";
+    }
+    if(b>a & b>c){
+        cout<<"b is greatest";
+    }
+    else{
+        cout<<"c is greatest";
+    }
+}
