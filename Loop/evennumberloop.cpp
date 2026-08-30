07e2fb490bbd5e6bd700dc1009f2f8b5ed4ab47f
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-08-30 14:46:05 +0530

    method change

--- Loop/evennumberloop.cpp
+++ Loop/evennumberloop.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=19;i<=5015;i++){ 
-        if(i%19==0){
-            cout<<i<<" ";
-        }  
+    // for(int i=19;i<=190;i++){ //loop work 172 times 
+    //     if(i%19==0){
+    //         cout<<i<<" ";
+    //     }  
+    // }
+    for(int i=19;i<=190;i+=19){ //loop work only 10 times
+        cout<<i<<" ";    
     }
 }
