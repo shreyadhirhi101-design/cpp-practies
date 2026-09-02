b10cc63d0f4cc3b2db63593ca48af6979c5a51aa
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-02 19:34:45 +0530

    geometric progression

---
+++ Loop/gp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"n : ";
+    cin>>n;
+    int b;
+    cout<<"b : ";
+    cin>>b;
+    int a = 4;
+    for(int i=1;i<=n;i++){
+        cout<<a<<" ";
+        a = a * b;
+    }
+}
