2b407fb9754f26b69556655313ba889c56bba20e
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-06 23:12:09 +0530

    Flipped Stars

--- Loop/Patterns/digonalstars.cpp
+++ Loop/Patterns/digonalstars.cpp
@@ -4,8 +4,8 @@ int main(){
     int m;
     cout<<"Number of Rows: "; //no of rows
     cin>>m;
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=i;j++){
+    for(int i=1;i>=m;i--){
+        for(int j=1;j>=i;j--){
             cout<<"* ";
         }
         cout<<endl;
