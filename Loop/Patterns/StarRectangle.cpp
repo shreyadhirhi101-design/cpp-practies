fb40e66d079642b013a750cb74c11b616a0c1606
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-06 22:00:38 +0530

    adding the function of number of columns

--- Loop/Patterns/StarRectangle.cpp
+++ Loop/Patterns/StarRectangle.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 int main(){
     int m;
-    cout<<"m : "; //no of rows
+    cout<<"Number of Rows: "; //no of rows
     cin>>m;
+    int n;
+    cout<<"Number of Cols : "; //no of cols
+    cin>>n;
     for(int i=1;i<=m;i++){
-        cout<<"*****"<<endl;
+        for(int i=1;i<=n;i++){
+            cout<<"*";
+        }
+        cout<<endl;
     }
 }
