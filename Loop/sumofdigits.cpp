8583f450521a7c7d04e6577462473e0cba07bf44
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-06 18:12:23 +0530

    Sum of Digits

---
+++ Loop/sumofdigits.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"n : ";
+    cin>>n;
+    int sum = 0;
+    while(n!=0){
+        int ld = n%10;
+        n/=10;
+        sum+=ld;
+    }
+    cout<<sum;
+}
