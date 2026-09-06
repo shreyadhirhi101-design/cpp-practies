1dbbb747bb0e66fab87341128af978708e75a7cc
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-06 22:56:18 +0530

    Digonal Stars

---
+++ Loop/Patterns/digonalstars.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Number of Rows: "; //no of rows
+    cin>>m;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
