ab5f3e0dd702c3720d5c0b5163fa5b32e7b78922
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-13 14:04:51 +0530

    with input

--- Loop/Patterns/Functions/Recursion/Arrays/syntaxdeclaration.cpp
+++ Loop/Patterns/Functions/Recursion/Arrays/syntaxdeclaration.cpp
@@ -3,13 +3,16 @@ using namespace std;
 int main(){
     // 5 integers 8, 4, 5 0, 1
     int arr[5]; //declaration
-    arr[0] = 8; //set
-    arr[1] = 4;
-    arr[2] = 5;
-    arr[3] = 0;
-    arr[4] = 1;
+    // arr[0] = 8; //set
+    // arr[1] = 4;
+    // arr[2] = 5;
+    // arr[3] = 0;
+    // arr[4] = 1;
+     for(int i=0;i<=4;i++){
+        cin>>arr[i];
+    }
     for(int i=0;i<=4;i++){
-        cout<<arr[i]<<" ";
+        cout<<arr[i]*2<<" ";
     }
     // cout<<arr[0]<<" ";
     // cout<<arr[1]<<" ";
