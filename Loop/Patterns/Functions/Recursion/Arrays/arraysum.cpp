f71a0d473ac56356ef6766f48e9b7b98e97d63a8
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-13 17:22:47 +0530

    with input

--- Loop/Patterns/Functions/Recursion/Arrays/arraysum.cpp
+++ Loop/Patterns/Functions/Recursion/Arrays/arraysum.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[] = {4, 5, 6, 2, 0};
+    int arr[5]; 
+    cout<<"Enter Array Elements : ";
+    for(int i=0;i<=4;i++){
+        cin>>arr[i];
+    }
     int sum = 0;
     for(int i=0;i<=4;i++){
         sum += arr[i];
