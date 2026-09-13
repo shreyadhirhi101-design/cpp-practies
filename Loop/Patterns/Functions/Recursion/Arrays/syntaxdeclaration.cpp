9558a242f4b9f90a36200fa5a89165353211159f
Shreya Dhirhi <shreyadhirhi101@gmail.com>
2026-09-13 14:13:29 +0530

    update

--- Loop/Patterns/Functions/Recursion/Arrays/syntaxdeclaration.cpp
+++ Loop/Patterns/Functions/Recursion/Arrays/syntaxdeclaration.cpp
@@ -4,17 +4,19 @@ int main(){
     // 5 integers 8, 4, 5 0, 1
     int arr[5]; //declaration
     cout<<"Enter Array Elements : "<<endl;
+    for(int i=0;i<=4;i++){
+        cin>>arr[i];
+    }
+    arr[0] = 13;
+    cout<<endl;
+    for(int i=0;i<=4;i++){
+        cout<<arr[i]<<" ";
+    }
     // arr[0] = 8; //set
     // arr[1] = 4;
     // arr[2] = 5;
     // arr[3] = 0;
     // arr[4] = 1;
-     for(int i=0;i<=4;i++){
-        cin>>arr[i];
-    }
-    for(int i=0;i<=4;i++){
-        cout<<arr[i]*2<<" ";
-    }
     // cout<<arr[0]<<" ";
     // cout<<arr[1]<<" ";
     // cout<<arr[2]<<" ";
